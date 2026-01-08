48205efdbb91cb2ad97a78ee0532741970db00ba
NEWS TEZ <68455421+medee404@users.noreply.github.com>
2026-01-08 23:16:19 +0530

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,8 @@
+
+/**
+ * न्यूज़ कहाँ से आएगी (News Source):
+ * अभी यह TechCrunch से न्यूज़ ले रहा है। 
+ * अगर आप अपनी वेबसाइट से न्यूज़ दिखाना चाहते हैं, तो नीचे दिए गए URL को बदलें।
+ * उदाहरण: 'https://yourwebsite.com/wp-json/wp/v2'
+ */
+export const WORDPRESS_API_URL = 'https://techcrunch.com/wp-json/wp/v2';
